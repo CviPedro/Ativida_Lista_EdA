5d9ef25b0bb768330dd14bb9e73794271663b5d2
CviPedro <135079780+CviPedro@users.noreply.github.com>
2025-09-16 22:09:13 -0300

    Implementação das listas em andamento e concluido

--- Main.java
+++ Main.java
@@ -1,19 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        TodoList lista = new TodoList();
-        lista.inserirNoInicio("matematica");
-        lista.inserirNoInicio("5*9");
-        lista.inserirNoFim("4+5");
-        lista.imprimirLista();
+        Trello aFazer = new Trello();
+        Trello concluido = new Trello();
+        Trello emAndamento = new Trello();
 
-        lista.inserir("5-8", 1);
-        lista.imprimirLista();
+        aFazer.inserirNoInicio("matematica");
+        aFazer.inserirNoInicio("5*9");
+        aFazer.inserirNoFim("4+5");
+        aFazer.imprimirLista();
 
-        lista.alteraElemento("4+9", 2);
-        lista.imprimirLista();
+        aFazer.inserir("5-8", 1);
+        aFazer.imprimirLista();
 
-        lista.remove(0);
-        lista.imprimirLista();
+        aFazer.alteraElemento("4+9", 2);
+        aFazer.imprimirLista();
 
+        aFazer.remove(0);
+        aFazer.imprimirLista();
+
+        aFazer.moverTarefa(emAndamento, 0);
+        System.out.println("\n* À fazer após * a movimentação:");
+        aFazer.imprimirLista();
+        System.out.println("\n* Em andamento * :");
+        emAndamento.imprimirLista();
+
+        emAndamento.moverTarefa(concluido, 0);
+        System.out.println("\n* Em andamento * após movimentação: ");
+        emAndamento.imprimirLista();
+        System.out.println("\n* Concluido * : ");
+        concluido.imprimirLista();
     }
 }
